99b4a84e43ded68d38c7b143a45335ceb94aa10a
DeeyBeey <dhruv_belai@yahoo.com>
2025-02-20 15:26:03 -0500

    implemented db, cors, sessions

--- server/config.js
+++ server/config.js
@@ -1,7 +1,11 @@
 const PORT = 5555;
-// const MONGO_URL = "mongodb://mongodb:27017/";
+const MONGO_URL = "mongodb://localhost:27017/paymate";
+const CLIENT_URL = "http://localhost:3000"
+const COOKIE_LENGTH = 30 * 24 * 60 * 60 * 1000;
 
 module.exports = {
     PORT,
-    // MONGO_URL
+    MONGO_URL,
+    CLIENT_URL,
+    COOKIE_LENGTH
 };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,39 @@
 const express = require("express");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const mongoose = require("mongoose");
+const cors = require("cors");
+const crypto = require("crypto");
+const SessionSecret = crypto.randomBytes(32).toString("hex");
 
-const { PORT } = require("./config.js")
+const { PORT, MONGO_URL, CLIENT_URL, COOKIE_LENGTH } = require("./config.js")
+
+mongoose.connect(MONGO_URL);
 
 const app = express()
+
+const store = new MongoDBStore({
+    uri: MONGO_URL,
+    collection: 'Sessions',
+})
+
+app.use(
+    cors({
+        credentials: true,
+        origin: [CLIENT_URL],
+    }),
+    session({
+        secret: SessionSecret,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: COOKIE_LENGTH
+        },
+        store: store,
+    })
+);
+
+app.use(express.json());
 
 app.get('', (request, response) =>{
     response.send("PayMate App Server");
