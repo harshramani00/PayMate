f4a32a232928a35d98fded55d7eea1355a117ea6
Aditya Bhanwadiya <adityabhanwadiya@adityas-mbp.mynetworksettings.com>
2025-03-30 21:39:40 -0500

    oauth done

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -1,8 +1,54 @@
- import React from 'react'
- 
- export default function OAuth() {
-   return (
-     <div>OAuth</div>
-   )
- }
- 
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+
+export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+
+      provider.setCustomParameters({
+        prompt: 'select_account',
+        display: 'popup'
+      });
+
+      const result = await signInWithPopup(auth, provider);
+
+      const res = await fetch('/server/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/scan-receipt');
+    } catch (error) {
+      console.log('could not sign in with google', error);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center mt-4">
+      <FcGoogle 
+        size={30}
+        className='cursor-pointer hover:opacity-80 transition-opacity'
+        onClick={handleGoogleClick}
+        role="button"
+        aria-label="Sign in with Google"
+        style={{ cursor: 'pointer' }} 
+      />
+      <div className="w-24 h-px bg-gradient-to-l from-transparent to-gray-300 black"></div>
+    </div>
+  );
+}
---
+++ frontend/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnIfeUXX8nu4cvt6g0cb4S48DzWSgGdl8",
+  authDomain: "paymate-2fb61.firebaseapp.com",
+  projectId: "paymate-2fb61",
+  storageBucket: "paymate-2fb61.firebasestorage.app",
+  messagingSenderId: "690549200026",
+  appId: "1:690549200026:web:a853b87561beb42bcb7b23",
+  measurementId: "G-JHV8WWLSSR"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
